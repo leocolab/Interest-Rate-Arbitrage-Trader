c015efca44a4d4364721b69b8ca0b24a3d4578e8
Leo Colabrese <leo.colabrese@yahoo.com>
2025-01-08 19:15:44 -0500

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,6 @@
+# CHANGE THESE VARIABLES BASED ON RATE DECISION DATE OF INTEREST
+betName = 'fed-interest-rates-january-2025' #title of polymarket bet name via URL
+dateTime = 'Jan 29, 2025 02:00PM ET'
+currentRateRange = '4.25 - 4.50'
+buyTokenId = ''
+sellTokenId = ''
